8c9fda9bfecea5342c8d58820571489ef874b9c8
yuliani.espitia <yuliani.espitia@bjungle.net>
2024-09-19 21:45:27 -0500

    primer commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { ContactoComponent } from './pages/contacto/contacto.component';
+import { RegistroUserComponent } from './pages/registro-user/registro-user.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'contacto',
+        component: ContactoComponent
+    },
+    {
+        path: 'registro',
+        component: RegistroUserComponent
+       
+    }
+];
